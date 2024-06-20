6ebf8904b6b1fbbb43ed3fdc8f59f4674292c0b4
CharlesOkechukwu <realcharlieok@gmail.com>
2024-06-20 17:14:54 +0000

    fix bad checher"

--- 0x06-unittests_in_js/6-payment_token.test.js
+++ 0x06-unittests_in_js/6-payment_token.test.js
@@ -6,10 +6,8 @@ describe('getPaymentTokenFromAPI', function() {
   it("return a data object data: successful response from API", function() {
     getPaymentTokenFromAPI(true)
     .then((res) => {
-      console.log(res)
-      expect(res.data).to.be.equal('Sucessful response from the API');
+      expect(res).to.deep.equal({data: 'Successful response from the API'});
       done();
-    })
-    .catch((err) => done(err));
+    });
   });
 });
