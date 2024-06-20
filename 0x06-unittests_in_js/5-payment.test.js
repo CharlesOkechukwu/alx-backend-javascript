10d16356cb8641593506e39fafb30ae651612762
CharlesOkechukwu <realcharlieok@gmail.com>
2024-06-20 16:00:12 +0000

    test with two various input

---
+++ 0x06-unittests_in_js/5-payment.test.js
@@ -0,0 +1,24 @@
+const chai = require('chai');
+const expect = chai.expect;
+const sinon = require('sinon');
+const Utils = require('./utils');
+const sendPaymentRequestToApi = require('./5-payment');
+
+describe('sendPaymentRequestToApi', function() {
+  beforeEach(function() {
+    sinon.spy(console, 'log');
+  });
+  afterEach(function() {
+    sinon.restore();
+  });
+  it("call with 100 and 20", function() {
+    sendPaymentRequestToApi(100, 20);
+    expect(console.log.calledOnce).to.be.true;
+    expect(console.log.calledWith('The total is: 120')).to.be.true;
+  });
+  it("call with 10 and 10", function() {
+    sendPaymentRequestToApi(10, 10);
+    expect(console.log.calledOnce).to.be.true;
+    expect(console.log.calledWith('The total is: 20')).to.be.true;
+  });
+});
